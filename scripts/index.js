802d3ce302e3d46909eebe06ba2f5d9366f1068d
Andrey Svirin <aaswirin@gmail.com>
2025-02-21 14:41:44 +0300

    Настройки для разметки

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,24 @@
 /** @module index */
 
+/**
+ * Настройки для разметки
+ * При изменении разметки править здесь
+ */
+const settings = {
+  idTemplate: "#card-template",                     // Id template'а для карт
+  classPlacesList: '.places__list',                 // Класс места для укладки карт
+  classPlacesItem: '.places__item',                 // Класс для карты
+  classCardImage: '.card__image',                   // Класс для картинки карты
+  classCardTitle: '.card__title',                   // Класс для подписи карты
+  classCardDeleteButton: '.card__delete-button',    // Класс для кнопки удаления карты
+  classListItem: 'li.places__item',                 // Тэг и Класс места для укладки карт
+}
+
 /** Заготовка */
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector(settings.idTemplate).content;
 
 /** DOM узлы по потребности */
-const placesContainer=document.querySelector('.places__list');     // Место для укладки карт
+const placesContainer=document.querySelector(settings.classPlacesList);     // Место для укладки карт
 
 /**
  * Создание карточки.
@@ -16,16 +30,16 @@ const placesContainer=document.querySelector('.places__list');     // Место
  * @returns {HTMLElement} карточка для размещения на странице
  */
 function addPlace(objectPlace, functionDelete) {
-  const newPlace = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const newPlace = cardTemplate.querySelector(settings.classPlacesItem).cloneNode(true);
 
   // Изображение
-  const cardImage = newPlace.querySelector('.card__image');
+  const cardImage = newPlace.querySelector(settings.classCardImage);
   cardImage.setAttribute('src', objectPlace.link);
   cardImage.setAttribute('alt', 'Место на картинке: ' + objectPlace.name);
   // Подпись
-  newPlace.querySelector('.card__title').textContent = objectPlace.name;
+  newPlace.querySelector(settings.classCardTitle).textContent = objectPlace.name;
   // Кнопка Удалить
-  newPlace.querySelector('.card__delete-button').addEventListener('click', functionDelete);
+  newPlace.querySelector(settings.classCardDeleteButton).addEventListener('click', functionDelete);
 
   return newPlace;
 }
@@ -39,7 +53,7 @@ const deletePlace = function (event) {
   if (event.target === null) return;
 
   // Поискать карточку выше и выше, вдруг враги разметку поменяли
-  const placeDelete = event.target.closest('li.places__item');
+  const placeDelete = event.target.closest(settings.classListItem);
 
   if (placeDelete === null) return;
 
